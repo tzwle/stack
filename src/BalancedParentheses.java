8d3ad94e2610082e270b209b7b880afdcb7c5a8a
tzwle <161288653+tzwle@users.noreply.github.com>
2024-03-17 10:19:50 +0800

    Update BalancedParentheses.java

--- src/BalancedParentheses.java
+++ src/BalancedParentheses.java
@@ -1,5 +1,36 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Stack;
+
+public class BalancedParentheses{
+
+    Stack<Charaacter> stack = new Stack<>();
+
+    public  void balancedParentheses(String expression) {
+        for(char bracket: expression.toCharArray()) {
+            if(bracket == '{' || bracket =='(' || bracket == '['){
+                stack.push(bracket);
+
+            }else if(bracket == '}' || bracket ==')' || bracket == ']'){
+                if(stack.isEmpty() || isItBalanced(stack.pop(), bracket)){
+                    System.out.println("The expression is not balanced");
+                    return;
+
+                }
+
+            }   
+        }
+        System.out.println("The experssion is balanced");       
     }
+
+    public boolean isItBalanced(char open, char close){
+
+        if(open == '{' && close == '}'){
+            return true;
+        }else if(open == '(' || close == ')'){
+            return true;
+        }else if(open == '[' || close == ']'){
+            return true;
+        }
+        return isBalanced;
+    }
+    
 }
